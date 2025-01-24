03d396617697422ac02d086963c67c7f698f8a8a
Gonzalo Gentile <gonza.gentile@gmail.com>
2025-01-24 20:00:34 +0100

    first push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Welcome to My React App!</h1>
+      <p>Deployed on GitHub Pages 🚀</p>
+      <button onClick={() => alert('Hello from React!')}>Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
