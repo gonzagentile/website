bf3781bc883785e08b5546d934eeb1ade6034b88
Gonzalo Gentile <gonza.gentile@gmail.com>
2025-01-25 20:10:12 +0100

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Welcome to My React App!</h1>
+      <h1>hola to My React App!</h1>
       <p>Deployed on GitHub Pages 🚀</p>
       <button onClick={() => alert('Hello from React!')}>Click Me!</button>
     </div>
