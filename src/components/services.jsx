0e0824bbb857494efbf293580c44f6a8d5e4eb5e
Gonzalo Gentile <gonza.gentile@gmail.com>
2025-01-30 20:27:40 +0100

    new code

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Services.css"; // Import the CSS file for styling
 
 export const Services = (props) => {
   return (
@@ -6,19 +7,23 @@ export const Services = (props) => {
       <div className="container">
         <div className="section-title">
           <h2>Our Services</h2>
-          <p>
-            This is a test.
-          </p>
+          <p>We offer a wide range of IT security services to protect your business.</p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
+                <div key={`${d.name}-${i}`} className="col-md-4 service-item">
                   <i className={d.icon}></i>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
+                    {d.subServices && (
+                      <ul>
+                        {d.subServices.map((subService, index) => (
+                          <li key={index}>{subService}</li>
+                        ))}
+                      </ul>
+                    )}
                   </div>
                 </div>
               ))
